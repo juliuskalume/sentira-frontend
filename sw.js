f9e9a2f2ff59e0fe83a0b1a8e2bca6748ba29b88
juliuskalume <148798597+juliuskalume@users.noreply.github.com>
2026-02-06 01:38:15 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "5gvci.com",
-    "zoneId": 10574949
+    "domain": "3nbf4.com",
+    "zoneId": 10574956
 }
 self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
